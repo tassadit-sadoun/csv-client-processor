a51908dc1646f0a2f220eedba750e63e2061fef4
tassadit <tassadite.sadoun@gmail>
2025-05-24 18:52:26 +0200

    add database folder

---
+++ api/database/db.py
@@ -0,0 +1,28 @@
+from typing import Generator
+
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from log_config.log import get_logger
+
+POSTGRES_URL = os.getenv("POSTGRES_URL")
+
+engine = create_engine(POSTGRES_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+log = get_logger(__name__)
+
+
+def db_context() -> Generator:
+    session = SessionLocal()
+    try:
+        yield session
+    except Exception as e:
+        log.error(
+            "An error occurred while getting the database session. Error: "
+            " %s", e
+        )
+    finally:
+        log.debug("closing database session")
+        session.close()
